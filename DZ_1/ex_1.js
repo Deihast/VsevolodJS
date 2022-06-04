2e10a7ffffd3b20ab1cd2a651a2aa784847cd4cf
Deihast <kekkon26@gmail.com>
2022-06-04 21:51:28 +0300

    dz_1

---
+++ DZ_1/ex_1.js
@@ -0,0 +1,11 @@
+/* FIRST TASK */
+
+const a = 50;
+let customBased = 'Something'; let constantBased = 43;
+
+console.log('Before: ',customBased, constantBased);
+
+customBased = 13;
+constantBased += a;
+
+console.log('Result: ',customBased, constantBased);
