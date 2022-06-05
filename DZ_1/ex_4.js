138007f8605d437f952f16f6fdd6837ed8898750
Deihast <kekkon26@gmail.com>
2022-06-05 13:19:51 +0300

    dz_1

---
+++ DZ_1/ex_4.js
@@ -0,0 +1,26 @@
+const a = '5';
+const b = 'w';
+const c = 12;
+let result1; let result2; let result3;
+
+result1 = a**2;
+result2 = b**2;
+result3 = c**2;
+
+if (result1 !== result1){
+    console.log('First result is NaN!');
+} else {
+    console.log ('Result: '+result1);
+}
+
+if (result2 !== result2){
+    console.log('Second result is NaN!');
+} else {
+    console.log ('Result: '+result2);
+}
+
+if (result3 !== result3){
+    console.log('Third result is NaN!');
+} else {
+    console.log ('Result: '+result3);
+}
