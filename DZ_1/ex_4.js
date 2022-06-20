96e696fc73939dfde93aaf22a3303f62e2e633f5
Deihast <kekkon26@gmail.com>
2022-06-20 12:11:40 +0300

    dz_1

--- DZ_1/ex_4.js
+++ DZ_1/ex_4.js
@@ -1,11 +1,10 @@
-let a = 5;
-let b = '12';
-let c = '5';
-let h = 'w';
-let j = null;
-let k = [];
-
-let d = 2; 
+const a = 5;
+const b = '12';
+const c = '5';
+const h = 'w';
+const j = null;
+const k = [];
+const d = 2; // multiplier
 
 console.log('First result is: \n')
 
@@ -13,30 +12,28 @@ if (typeof a === 'number' || typeof a === 'string'){
     let result1;
     result1 = a**d;
     if(result1 !== result1){
-        console.log('Its NaN. Try again.')
+        console.log('Its NaN. Try again.\n')
     } else{
         console.log(result1);
     }
 } else {
-    console.log('You cant do this!');
+    console.log('You cant do this!\n');
 }
 
-console.log();
 console.log('Second result is: \n')
 
-if (typeof h === 'number' || typeof h === 'string'){
+if (typeof j === 'number' || typeof j === 'string'){
     let result1;
-    result1 = h**d;
+    result1 = j**d;
     if(result1 !== result1){
-        console.log('Its NaN. Try again.')
+        console.log('Its NaN. Try again.\n')
     } else{
         console.log(result1);
     }
 } else {
-    console.log('You cant do this!');
+    console.log('You cant do this!\n');
 }
 
-console.log();
 console.log('Third result is: \n')
 if (typeof b === 'number' || typeof b === 'string'){
     let result1;
